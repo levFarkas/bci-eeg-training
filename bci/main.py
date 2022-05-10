feat: baseline normalization + fixes label values

bci/main.py
@@ -1,5 +1,4 @@
-import matplotlib.pyplot as plt
-
+import numpy as np
 
 from typing import Dict, Tuple
 
@@ -23,8 +22,15 @@ class Handler:
             else self._load_engine.load_all_data("../resources/files/eegmmidb/1.0.0")
 
         featured_data = self._convert_to_featured_data(data, csp=False)
-        train_features, test_features, train_labels, test_labels = self._feature_service.get_test_and_train_data(featured_data)
+        train_features, test_features, train_labels, test_labels = self._feature_service.get_test_and_train_data(
+            featured_data)
         neural_network = NeuralNetwork()
+
+        train_labels = np.concatenate(train_labels)
+        train_features = np.concatenate(train_features)
+        test_labels = np.concatenate(test_labels)
+        test_features = np.concatenate(test_features)
+
         neural_network.train(
             train_labels=train_labels,
             train_features=train_features,
@@ -34,7 +40,6 @@ class Handler:
 
     def _convert_to_featured_data(self, data: Dict[str, Tuple[EEGData, EEGData]], csp: bool) -> Dict[str, tuple]:
         return {key: self._feature_service.feature_extraction(data[key], csp) for key in data}
-
 
 
 if __name__ == '__main__':

bci/service/feature_service.py
@@ -1,3 +1,4 @@
+import copy
 from typing import Dict
 
 import numpy as np
@@ -9,17 +10,13 @@ from bci.model.eeg_data import EEGData, BOTH_FISTS_AND_BOTH_FEET_EXERCISES
 
 class FeatureService:
 
-    @staticmethod
-    def feature_extraction(eeg_tuple: (EEGData, EEGData), csp: bool) -> tuple:
+    def feature_extraction(self, eeg_tuple: (EEGData, EEGData), csp: bool) -> tuple:
         result_x, result_y = [], []
         for eeg_data in eeg_tuple:
-            y = eeg_data.epochs.events[:, -1] - 2
-
-            if eeg_data.exercise_number in BOTH_FISTS_AND_BOTH_FEET_EXERCISES:
-                y += 2
+            y = eeg_data.epochs.events[:, -1]
 
             epochs_data = eeg_data.epochs.get_data()
-            x = epochs_data
+            x = self._normalize_data(epochs_data)
             if csp:
                 csp = CSP(n_components=4, log=True, reg=None)
 
@@ -35,3 +32,16 @@ class FeatureService:
         X = [value[0] for value in featured_data.values()]
         Y = [value[1] for value in featured_data.values()]
         return train_test_split(X, Y, train_size=0.80, test_size=0.20)
+
+    @staticmethod
+    def _normalize_data(epochs_data: np.ndarray):
+        new_epochs_data = copy.deepcopy(epochs_data)
+        x, y, z = new_epochs_data.shape
+        for i in range(z):
+            for j in range(y):
+                row = new_epochs_data[:, j, i]
+                normalized_row = row / np.linalg.norm(row)
+                # normalized_row = row / np.std(row, axis=0)
+                new_epochs_data[:, j, i] = normalized_row
+
+        return new_epochs_data